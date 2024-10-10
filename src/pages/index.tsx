4c96f914a11b443e6411c3a811804ce6150eb446
lokiwonder <loki@wondermove.net>
2024-10-10 20:47:36 +0900

    page rendering error fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { DASHBOARD_PATH, LOGIN_PATH } from '@/common/constants';
 const Page = () => {
   const router = useRouter();
   useEffect(() => {
+    console.log('test');
     const lowercasePath = router.pathname.toLowerCase();
     if (lowercasePath === '/') {
       router.replace(LOGIN_PATH);
